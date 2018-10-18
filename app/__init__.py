97b0976a1f37adf34cfdabfb9e744f116b9e80f8
Allan069 <allannelly690@gmail.com>
2018-10-18 11:36:23 +0300

    [chore 161272132]changed app settings to run with development env settings

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
+"""Controller file that registers the blueprints of our application and creates
+the flask application"""
 from flask import Flask
 from app.api.v1.views.product_views import prod as product_bp
 from app.api.v1.views.sale_views import sal as sales_bp
 from app.api.v1.views.user_view import user_dec as user_bp
-flaskApp = Flask(__name__)
-flaskApp.config.from_object('development')
-
-flaskApp.register_blueprint(product_bp)
-flaskApp.register_blueprint(sales_bp)
-flaskApp.register_blueprint(user_bp)
+# create our flask application
+flask_app = Flask(__name__)
+# register the blueprints of our models
+flask_app.register_blueprint(product_bp)
+flask_app.register_blueprint(sales_bp)
+flask_app.register_blueprint(user_bp)
