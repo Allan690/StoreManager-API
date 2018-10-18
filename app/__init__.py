d475db5acdf90e5e6ed6d69fcb75d572fec58a8b
Allan069 <allannelly690@gmail.com>
2018-10-18 07:03:20 +0300

    [fix #161285552]added blueprint for user endpoints in the controller file

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
+import os
 from flask import Flask
-from app.api.v1.views.product_views import prod
-from app.api.v1.views.sale_views import sal
+from app.api.v1.views.product_views import prod as product_bp
+from app.api.v1.views.sale_views import sal as sales_bp
+from app.api.v1.views.user_view import user_dec as user_bp
 flaskApp = Flask(__name__)
+flaskApp.secret_key = os.getenv('SECRET')
 # app.config.from_object('config.Development')
 
-flaskApp.register_blueprint(prod)
-flaskApp.register_blueprint(sal)
+flaskApp.register_blueprint(product_bp)
+flaskApp.register_blueprint(sales_bp)
+flaskApp.register_blueprint(user_bp)
