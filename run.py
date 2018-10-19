0726f25180d9c6056b53681d91ca7e97b13901df
Allan069 <allannelly690@gmail.com>
2018-10-19 13:03:50 +0300

    [feature #161331831]added secret key to the app entry point

--- run.py
+++ run.py
@@ -6,5 +6,6 @@ flask_app.config.update(
     DEBUG=True,
     APP_SETTINGS=app_config['development']
 )
+flask_app.secret_key = 'hello-there-its-allan'
 if __name__ == '__main__':
     flask_app.run()
