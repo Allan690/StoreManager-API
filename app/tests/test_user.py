4a0a7cf68b42ee22174da4c43521f357d319a535
Allan069 <allannelly690@gmail.com>
2018-10-19 13:15:06 +0300

    [feature #161331831]Refactored tests for the user endpoints

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -10,8 +10,8 @@ class UserLoginClass(TestSetUp):
     def test_user_can_register(self):
         """Test if API can add new user"""
         response = self.app.post(
-            "/api/v1/register",
-            data=json.dumps(self.user),
+            "/api/v1/auth/register",
+            data=json.dumps(dict(email="testuser1@gmail.com", password="testpass11234")),
             content_type="application/json")
         self.assertEqual(response.status_code, 201)
         response_msg = json.loads(response.data.decode("UTF-8"))
@@ -19,7 +19,7 @@ class UserLoginClass(TestSetUp):
 
     def test_blank_email(self):
         """Tests if error is raised when email is missing."""
-        response = self.app.post("/api/v1/register",
+        response = self.app.post("/api/v1/auth/register",
                                  data=json.dumps(self.missing_email),
                                  content_type="application/json")
         self.assertEqual(response.status_code, 400)
@@ -27,9 +27,9 @@ class UserLoginClass(TestSetUp):
         self.assertIn("required", response_msg["Message"])
 
     def test_missing_password(self):
-        """Tests error raised when password is missing."""
-        response = self.app.post("/api/v1/register",
-                                 data=json.dumps(self.missing_password),
+        """Tests if error is raised when password is missing."""
+        response = self.app.post("/api/v1/auth/register",
+                                 data=json.dumps(dict(email="testuser2@gmail.com", password="")),
                                  content_type="application/json")
         self.assertEqual(response.status_code, 400)
         response_msg = json.loads(response.data.decode("UTF-8"))
@@ -37,7 +37,7 @@ class UserLoginClass(TestSetUp):
 
     def test_wrong_email_format(self):
         """Tests error raised when wrong email format is provided."""
-        response = self.app.post("/api/v1/register",
+        response = self.app.post("/api/v1/auth/register",
                                  data=json.dumps(self.wrong_email_format),
                                  content_type="application/json")
         self.assertEqual(response.status_code, 400)
@@ -45,14 +45,75 @@ class UserLoginClass(TestSetUp):
         self.assertIn("Wrong", response_msg["Message"])
 
     def test_get_all_users(self):
-        """Test if get method gets all registered users"""
-        response = self.app.get('/api/v1/users',
-                                content_type="application/json")
+        """Test if API can get all registered users of the app"""
+        response = self.app.get('/api/v1/auth/users',
+                                content_type="application/json",
+                                headers={"x-access-token": self.token})
         self.assertEqual(response.status_code, 200)
 
+    def test_valid_login_generates_auth_token(self):
+        """Tests token is generated on successful login."""
+        response = self.app.post("/api/v1/auth/login",
+                                 data=json.dumps(self.user),
+                                 content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("token", response_msg)
+
+    def test_user_can_reset_password(self):
+        # tests if the API can change a user's password
+        """Register a user"""
+        self.app.post('/api/v1/auth/register', data=json.dumps(self.user),
+                      headers={"content-type": "application/json"})
+
+        # login the just registered user and get a token
+        self.login = self.app.post(
+            '/api/v1/auth/login',
+            data=json.dumps(
+                self.user),
+            content_type='application/json')
+        self.data = json.loads(self.login.get_data(as_text=True))
+        self.token = self.data['token']
+        # Then reset their password with the new token
+        response = self.app.put(
+            '/api/v1/auth/reset-password',
+            data=json.dumps(
+                dict(
+                    password="new_pass")),
+            content_type="application/json",
+            headers={
+                "x-access-token": self.token})
+        self.assertEqual(response.status_code, 202)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("updated", response_msg["Message"])
+
+    def test_valid_logout(self):
+        """Tests for a valid user logout"""
+        response = self.app.delete(
+            '/api/v1/auth/logout',
+            headers={
+                "x-access-token": self.token})
+        self.assertEqual(response.status_code, 200)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("out", response_msg["Message"])
+
+    def test_repeat_logout(self):
+        """Test that API should prevent a user from logging out twice"""
+        self.app.delete(
+            '/api/v1/auth/logout',
+            headers={
+                "x-access-token": self.token})
+        response = self.app.delete(
+            '/api/v1/auth/logout',
+            headers={
+                "x-access-token": self.token})
+        self.assertEqual(response.status_code, 400)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("Already", response_msg["Message"])
+
     def tearDown(self):
-        """ clear data after every test"""
-        user_object.users.clear()
+            """ clear data after every test"""
+            user_object.users.clear()
 
 
 if __name__ == '__main__':
